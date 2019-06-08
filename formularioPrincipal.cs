813a25e644b36b3543a1e90a9e0e447b19e707ed
guillermo <guillermobrs@gmail.com>
2019-06-08 16:20:16 -0600

    Agregado de Imagenes y primeras ventanas principales

--- formularioPrincipal.cs
+++ formularioPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LaboratorioIsssSimulacion
 {
-    public partial class Form1 : Form
+    public partial class formularioPrincipal : Form
     {
-        public Form1()
+        public formularioPrincipal()
         {
             InitializeComponent();
         }
@@ -20,6 +20,12 @@ namespace LaboratorioIsssSimulacion
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BotonMostrarPantallaSimulacion(object sender, EventArgs e)
+        {
+            formularioSimulacion simulacion = new formularioSimulacion();
+            simulacion.Show();
         }
     }
 }
